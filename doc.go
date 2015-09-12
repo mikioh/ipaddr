1f031fbd235ce7c5ca3f8a602e5c1adfa9701ffe
Mikio Hara <mikioh.mikioh@gmail.com>
2015-09-13 08:27:19 +0900

    ipaddr: update documnetation

--- doc.go
+++ doc.go
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE.
 
-// Package ipaddr provides basic functions for the manipulation of IP
-// address prefixes and subsequent addresses as described in RFC 4632
-// and RFC 4291.
+/*
+Package ipaddr provides basic functions for the manipulation of IP
+address prefixes and subsequent addresses as described in RFC 4632 and
+RFC 4291.
+*/
 package ipaddr
